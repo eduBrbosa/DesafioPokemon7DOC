52b97bdcb3cc8532ea7cabdb664017ac7d3b2180
eduBrbosa <d.dbarbosa142@gmail.com>
2025-11-14 14:22:44 -0300

    Iniciando projeto e fazendo a leitura da API 'PokeApi'

---
+++ Desafio7DOC/Program.cs
@@ -0,0 +1,43 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Desafio7DOC
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync("https://pokeapi.co/api/v2/pokemon/1");
+                    response.EnsureSuccessStatusCode();
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(content);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Pokemon
+    {
+        
+
+    }
+
+
+
+}
