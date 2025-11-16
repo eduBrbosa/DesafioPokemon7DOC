3de7b79dd13f96ccec56ac311629bfdcd03c34d6
eduBrbosa <d.dbarbosa142@gmail.com>
2025-11-16 16:30:03 -0300

    Deserializando o JSON da API, criando as classes DTO e Data Object das habilidades e listando as informações de acordo com o resultado da API

--- Desafio7DOC/Program.cs
+++ Desafio7DOC/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Desafio7DOC
@@ -10,15 +12,30 @@ namespace Desafio7DOC
     {
         static async Task Main(string[] args)
         {
+
+            Console.WriteLine("Escolha o seu Pokémon");
+            string pokemonEscolhido = Console.ReadLine();
+            Console.Clear();
             try
             {
                 using (var httpClient = new HttpClient())
                 {
-                    var response = await httpClient.GetAsync("https://pokeapi.co/api/v2/pokemon/1");
+                    var response = await httpClient.GetAsync($"https://pokeapi.co/api/v2/pokemon/{pokemonEscolhido}");
                     response.EnsureSuccessStatusCode();
                     var content = await response.Content.ReadAsStringAsync();
 
-                    Console.WriteLine(content);
+                    var pokemon = JsonSerializer.Deserialize<Pokemon>(content);
+
+                    Console.WriteLine($"INFORMAÇÕES DE {pokemonEscolhido}");
+                    Console.WriteLine($"Nome do Pokémon: {pokemon.Nome.ToUpper()}\n" +
+                                      $"Altura: {pokemon.Altura}\n" +
+                                      $"Peso: {pokemon.Peso}\n" +
+                                      $"HABILIDADES:\n");
+
+                    foreach (var habilidade in pokemon.Habilidades)
+                    {
+                        Console.WriteLine(habilidade.InfoHabilidade.NomeHabilidade.ToUpper());
+                    }
 
                 }
 
@@ -34,10 +51,28 @@ namespace Desafio7DOC
 
     public class Pokemon
     {
-        
+        [JsonPropertyName("name")]
+        public string Nome { get; set; }
 
+        [JsonPropertyName("height")]
+        public int Altura { get; set; }
+
+        [JsonPropertyName("weight")]
+        public int Peso { get; set; }
+
+        [JsonPropertyName("abilities")]
+        public List<HabilidadePokemon> Habilidades { get; set; }
     }
 
+    public class HabilidadePokemon
+    {
+        [JsonPropertyName("ability")]
+        public HabilidadeInfo InfoHabilidade { get; set; }
+    }
 
-
+    public class HabilidadeInfo
+    {
+        [JsonPropertyName("name")]
+        public string NomeHabilidade { get; set; }
+    }
 }
